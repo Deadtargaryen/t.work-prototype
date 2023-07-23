8d9c43fe1238380cc292be5fd0793c758081fc16
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-07-23 08:11:58 +0100

    updated Home.scss, Home.jsx, Navbar.scss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,6 +15,44 @@ const Home = () => {
           <CatCard key={card.id} item={card}/>
         ))}
       </Slide>
+      <div className="features">
+        <div className="container">
+          <div className="item">
+          <h1>Unlock a world of freelance talent, tailored for students like you!</h1>
+          <div className="title">
+            <img src="/public/img/check.png" alt="" />
+            Top-notch quality, regardless of your budget!
+          </div>
+            <p>
+              Quality services, fixed project-based pricing, no hourly rates!
+            </p>
+          <div className="title">
+            <img src="/public/img/check.png" alt="" />
+            Top-notch quality, regardless of your budget!
+          </div>
+            <p>
+              Quality services, fixed project-based pricing, no hourly rates!
+            </p>
+          <div className="title">
+            <img src="/public/img/check.png" alt="" />
+            Top-notch quality, regardless of your budget!
+          </div>
+            <p>
+              Quality services, fixed project-based pricing, no hourly rates!
+            </p>
+          <div className="title">
+            <img src="/public/img/check.png" alt="" />
+            Top-notch quality, regardless of your budget!
+          </div>
+            <p>
+              Quality services, fixed project-based pricing, no hourly rates!
+            </p>
+          </div>
+            <div className="item">
+              <video src="https://ik.imagekit.io/twork/typing_-_1655%20(360p).mp4?updatedAt=1690067076182" controls></video>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
