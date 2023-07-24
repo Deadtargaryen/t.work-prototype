2419b1fbc5e24ca86919db8b6dbf0f4124204206
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-07-24 13:08:45 +0100

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,9 @@ import "./Home.scss"
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/slide/Slide'
-import {cards} from '../../data'
+import {cards, projects} from '../../data'
 import CatCard from '../../components/catCard/CatCard'
+import ProjectCard from '../../components/projectCard/ProjectCard'
 const Home = () => {
   return (
     <div className='home'>
@@ -76,6 +77,11 @@ const Home = () => {
             </div>
         </div>
       </div>
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map(card=>(
+          <ProjectCard key={card.id} item={card}/>
+        ))}
+      </Slide>
     </div>
   )
 }
