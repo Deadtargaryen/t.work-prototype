31b6326c1e313cd37da4c92ff3662583132f05b3
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-08-25 07:45:24 +0100

    ..

--- api/server.js
+++ api/server.js
@@ -1,17 +1,22 @@
 /* eslint-disable no-undef */
-import express from "express";
-import mongoose from "mongoose";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 mongoose.set('strictQuery', true)
 
-try{
-    await mongoose.connect(port.env.MONGO);
-console.log('Connected to MongoDB!')
-} catch(error){
-handleError(error);
+const connect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB!')
+    } catch(error){
+    console.log(error);
+    }
 }
 
 app.listen(8800, () => {
+    connect()
 console.log('Backend server is running!')
 })
