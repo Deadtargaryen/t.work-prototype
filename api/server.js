193107f72842f6cbec3e12d97b936d6350cdf0c5
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-09-23 02:08:18 +0100

    routes, controllers

--- api/server.js
+++ api/server.js
@@ -3,6 +3,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoute from './routes/user.route.js'
+import gigRoute from './routes/gig.route.js'
+import messageRoute from './routes/message.route.js'
+import conversationRoute from './routes/conversation.route.js'
+import orderRoute from './routes/order.route.js'
+import reviewRoute from './routes/review.route.js'
 
 const app = express()
 dotenv.config()
@@ -18,6 +23,11 @@ const connect = async ()=>{
 }
 
 app.use('/api/users', userRoute)
+app.use('/api/gigs', gigRoute)
+app.use('/api/message', messageRoute)
+app.use('/api/conversation', conversationRoute)
+app.use('/api/order', orderRoute)
+app.use('/api/review', reviewRoute)
 
 app.listen(8800, () => {
     connect()
