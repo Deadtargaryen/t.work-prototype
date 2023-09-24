500327e514d801896f5883713ceba9eda14a58a3
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-09-24 23:08:10 +0100

    ..

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,13 @@
 import User from '../models/user.model.js'
-
+import bcrypt from 'bcrypt'
 export const register = async (req,res)=>{
 
     try{
-        const newUser = new User (req.body)
+        const hash = bcrypt.hashSync(req.body.password, 5)
+        const newUser = new User ({
+            ...req.body,
+            password: hash,
+        })
 
         await newUser.save()
         res.status(201).send('User created Successfully')
@@ -12,7 +16,19 @@ export const register = async (req,res)=>{
     }
 }
 export const login = async (req,res)=>{
-   
+    try{
+        const user = await User.findOne({username:req.body.username})
+        if(!user) return res.status(404).send('User not found!')
+
+        const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+        if(!isCorrect) return res.status(400).send('Wrong Password or username!')
+
+        const {password, ...info} = user._doc
+        res.status(200).send(info)
+
+    }catch(err){
+        res.status(500).send('something went wrong!')
+    }
 }
 export const logout = async (req,res)=>{
    
