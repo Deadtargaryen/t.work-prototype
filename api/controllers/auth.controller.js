154d29425e33b4214b8c0adcfbc6aeaa15698484
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-02 09:51:13 +0100

    clear cookie(logout)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -42,5 +42,8 @@ export const login = async (req, res, next)=>{
     }
 }
 export const logout = async (req,res)=>{
-   
+   res.clearCookie('accessToken', {
+    sameSite: 'none',
+    secure: true
+   }).status(200).send('User has been logged out.')
 }
