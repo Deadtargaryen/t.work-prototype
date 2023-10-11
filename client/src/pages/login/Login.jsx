6a90a54ddc391cf7c563bd355209d57672f2e271
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-11 08:37:03 +0100

    axios

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.scss"
+import axios from 'axios'
+
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+
+const handleSubmit = async (e)=>{
+  e.preventDefault()
+  const res = await axios.post('localhost:8800/api/auth/login', {username, password})
+}
+
   return (
     <div className='login'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Sign in</h1>
         <label htmlFor=''>Username</label>
-        <input name='Username' type="text" placeholder='johndoe'/>
+        <input name='Username' type="text" placeholder='johndoe' onChange={e=>setUsername(e.target.value)}/>
 
         <label htmlFor=''>Password</label>
-        <input type="password" name="Password" placeholder='********' />
+        <input type="password" name="Password" placeholder='********' onChange={e=>setPassword(e.target.value)}/>
 
         <button type='submit'>Login</button>
       </form>
