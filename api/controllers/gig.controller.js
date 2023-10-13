9c0ac31d2ed1e8746a7ff933a092fd39d7e37413
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-13 22:16:53 +0100

    hmm...

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,3 +1,27 @@
-export const fn = (req,res)=>{
-   
+import gig from '../models/gig.model'
+import createError from '../utils/createError'
+
+export const createGig = async (req, res, next) => {
+if (!req.isSeller) return next(createError(403, 'Only sellers can create a gig!'))
+
+const newGig = new Gig ({
+    userId: req.userId,
+    ...req.body
+})
+
+    try {
+        const savedGig = await newGig.save()
+        res.status(201).json(savedGig)
+    } catch (err) {
+        next (err)
+    }
+}
+export const deleteGig = async (req, res, next) => {
+    
+}
+export const getGig = async (req, res, next) => {
+
+}
+export const getGigs = async (req, res, next) => {
+
 }
--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import { verifyToken } from '../middleware/jwt.js'
 
 const router =  express.Router()
 
-router.get('/test', )
+router.post('/', verifyToken, createGig)
+router.delete('/:id', verifyToken, deleteGig)
+router.get('/single/:id', verifyToken, createGig)
+router.get('/', verifyToken, getGig)
+router.get('/', verifyToken, getGigs)
 
 export default router
