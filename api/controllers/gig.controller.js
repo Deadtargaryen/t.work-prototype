39334fd43e7884ef5547f94391411b28d2458dad
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-14 11:03:13 +0100

    import fn

--- api/controllers/gig.controller.js
+++ api/controllers/gig.controller.js
@@ -1,12 +1,12 @@
-import gig from '../models/gig.model'
-import createError from '../utils/createError'
+import Gig from '../models/gig.model.js'
+import createError from '../utils/createError.js'
 
 export const createGig = async (req, res, next) => {
 if (!req.isSeller) return next(createError(403, 'Only sellers can create a gig!'))
 
 const newGig = new Gig ({
     userId: req.userId,
-    ...req.body
+    ...req.body,
 })
 
     try {
--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,5 +1,9 @@
 import express from 'express'
 import { verifyToken } from '../middleware/jwt.js'
+import { createGig } from '../controllers/gig.controller.js'
+import { deleteGig } from '../controllers/gig.controller.js'
+import { getGig } from '../controllers/gig.controller.js'
+import { getGigs } from '../controllers/gig.controller.js'
 
 const router =  express.Router()
 
