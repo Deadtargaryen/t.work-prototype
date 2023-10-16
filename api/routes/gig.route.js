2f8a67a32401014c715904a2cdb5706f9014b842
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-16 07:06:15 +0100

    .error correction

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -9,8 +9,7 @@ const router =  express.Router()
 
 router.post('/', verifyToken, createGig)
 router.delete('/:id', verifyToken, deleteGig)
-router.get('/single/:id', verifyToken, createGig)
-router.get('/', verifyToken, getGig)
-router.get('/', verifyToken, getGigs)
+router.get('/single/:id', getGig)
+router.get('/', getGigs)
 
 export default router
