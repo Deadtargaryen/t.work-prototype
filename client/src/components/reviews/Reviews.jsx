ef48b4ab4f236772556a6965b363795047836565
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-10-22 15:14:01 +0100

    review settings

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Reviews.scss'
 import Review from '../review/Review'
+import { useQuery } from '@tanstack/react-query'
+import newRequest from '../../utils/newRequest'
 
-const Reviews = () => {
+const Reviews = ({gigId}) => {
+
+    const { isLoading, error, data} = useQuery({
+        queryKey: ['reviews'],
+        queryFn: () =>
+          newRequest.get(`/reviews/${gigId}`
+          ).then(res=>{
+            return res.data
+          })
+      })
+
   return (
     <div className="reviews">
             <h2>Reviews</h2>
-            <Review/>
+            {isLoading ? 'loading...'
+            : error ? 'Something went wrong!' 
+            : data.map((review)=> <Review key = {review._id} review = {review}/>)}
+            
           </div>
   )
 }
