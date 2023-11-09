c661d661727445fef96c46b999bfc6191838d1e6
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-09 09:20:23 +0100

    order, convo

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,3 +1,14 @@
-export const fn = (req,res)=>{
-   
+import createError from "../utils/createError"
+import Order from '../models/order.model.js'
+
+export const createOrder = async (req, res, next)=>{
+    try {
+        
+        const newOrder = new Order ({
+
+        })
+
+    } catch (err) {
+        next(err)
+    }
 }
