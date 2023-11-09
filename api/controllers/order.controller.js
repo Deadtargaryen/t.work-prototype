408b15787aea1dce0b7a1a0c3c8f27cfb43d45f4
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-09 10:06:00 +0100

    ord.cont

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,11 +1,20 @@
-import createError from "../utils/createError"
+import createError from "../utils/createError.js"
 import Order from '../models/order.model.js'
+import Gig from '../models/gig.model.js'
 
 export const createOrder = async (req, res, next)=>{
     try {
+
+        const gig = Gig.findById(req.params.gigId)
         
         const newOrder = new Order ({
-
+            gigId: gig._id,
+            img: gig.cover,
+            title: gig.title,
+            buyerId: req.userId,
+            sellerId: gig.userId,
+            price: gig.price,
+            payment_intent: 'temporary',
         })
 
     } catch (err) {
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -4,7 +4,7 @@ import { createOrder, getOrders } from '../controllers/order.controller.js'
 
 const router =  express.Router()
 
-router.post('/', verifyToken, createOrder )
+router.post('/:gigId', verifyToken, createOrder )
 router.get('/', verifyToken, getOrders )
 
 export default router
