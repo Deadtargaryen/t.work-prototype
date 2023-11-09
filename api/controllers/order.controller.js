d64391da531e09afe5882bbf23ea2ab8ff359766
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-09 11:02:01 +0100

    compleeted

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -5,7 +5,7 @@ import Gig from '../models/gig.model.js'
 export const createOrder = async (req, res, next)=>{
     try {
 
-        const gig = Gig.findById(req.params.gigId)
+        const gig = await Gig.findById(req.params.gigId)
         
         const newOrder = new Order ({
             gigId: gig._id,
@@ -16,6 +16,23 @@ export const createOrder = async (req, res, next)=>{
             price: gig.price,
             payment_intent: 'temporary',
         })
+
+        await newOrder.save()
+        res.status(200).send('stripe-id-successful')
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getOrders = async (req, res, next)=>{
+    try {
+         
+        const orders = await Order.find({
+            ...(req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }),
+            isCompleted: true,
+        })
+
+        res.status(200).send(orders)
 
     } catch (err) {
         next(err)
