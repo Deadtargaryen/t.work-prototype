9d59d528896627c0278ec5fe3c74b958bac1f7da
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-10 12:46:24 +0100

    convo

--- api/controllers/conversation.controller.js
+++ api/controllers/conversation.controller.js
@@ -1,7 +1,60 @@
-import createError from "../utils/createError"
+import createError from '../utils/createError.js'
+import Conversation from '../models/conversation.model.js'
 
-export const createOrder = async (req, res, next)=>{
+export const createConversation = async (req, res, next)=>{
+    
+    const newConversation = new Conversation({
+        id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userId,
+        sellerId: req.isSeller ? req.userId : req.body.to,
+        buyerId: req.isSeller ?  req.body.to : req.userId,
+        readBySeller: req.isSeller,
+        readByBuyer: !req.isSeller
+    })
+
     try {
+        const saveConversation = await newConversation.save()
+        res.status(201).send(saveConversation)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getConversations = async (req, res, next)=>{
+    try {
+        
+        const conversations = await Conversation.find(
+            req.isSeller ? {sellerId: req.userId} : {buyerId: req.userId}
+            )
+            res.status(200).send(conversations)
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getSingleConversation = async (req, res, next)=>{
+    try {
+        
+        const conversation = await Conversation.findOne({id: req.params.id})
+        res.status(200).send(conversation)
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const updateConversation = async (req, res, next)=>{
+
+    try {
+        const updatedConversation = await Conversation.findOneAndUpdate({id: req.params.id}, {
+            $set: {
+                readBySeller: req.isSeller,
+                readByBuyer: !req.isSeller,
+            },
+        },
+        { new: true }
+        )
+    
+        res.status(200).send(updatedConversation)
         
     } catch (err) {
         next(err)
--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,10 +1,16 @@
 import express from 'express'
+import { 
+    getConversations, 
+    createConversation, 
+    getSingleConversation,
+    updateConversation } from '../controllers/conversation.controller.js'
+import { verifyToken } from '../middleware/jwt.js'
 
 const router =  express.Router()
 
-router.get('/', verifyToken, getConversations ),
-router.get('/', verifyToken, createConversation ),
-router.get('/single/:id', verifyToken, getSingleConversation ),
+router.get('/', verifyToken, getConversations )
+router.get('/', verifyToken, createConversation )
+router.get('/single/:id', verifyToken, getSingleConversation )
 router.get('/:id', verifyToken, updateConversation )
 
 export default router
