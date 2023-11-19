9c7463e742d6ef9078c88db4fbb0277bc93e01c4
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-19 07:59:21 +0100

    msg.con...update

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,16 +1,26 @@
-import createError from "../utils/createError"
+import createError from "../utils/createError.js"
+import Message from '../models/message.model.js'
+import Conversation from '../models/conversation.model.js'
 
 export const createMessage = async (req, res, next)=>{
+    const newMessage = new Message({
+        ConversationId : req.body.conversationId,
+        userId: req.userId,
+        desc: req.body.desc
+    })
     try {
         
+        const savedMessage = await newMessage.save()
+        await Conversation.findOneAndUpdate({id: req.body.ConversationId})
     } catch (err) {
         next(err)
     }
 }
 export const getMessages = async (req, res, next)=>{
     
-    const messages
     try {
+        const messages = await Message.find({ConversationId : req.params.id})
+        res.status(200).send(messages)
         
     } catch (err) {
         next(err)
