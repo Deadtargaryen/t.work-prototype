46d90dbb2800971e54abd65efc5331cb551e4b94
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-25 23:39:25 +0100

    pay_STRIPE

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -1,6 +1,13 @@
 import createError from "../utils/createError.js"
 import Order from '../models/order.model.js'
 import Gig from '../models/gig.model.js'
+import Stripe from 'stripe';
+
+export const intent = async (req, res, next) =>{
+    const stripe = new Stripe(
+        process.env.STRIPE
+    )
+}
 
 export const createOrder = async (req, res, next)=>{
     try {
