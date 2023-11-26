53ce0524bf7a4175e3d2816b966c1b37b829fc59
Manny Etukudoh <mannyetukudoh@gmail.com>
2023-11-26 18:26:56 +0100

    ...payint

--- api/controllers/order.controller.js
+++ api/controllers/order.controller.js
@@ -37,27 +37,6 @@ export const intent = async (req, res, next) =>{
 
 }
 
-export const createOrder = async (req, res, next)=>{
-    try {
-
-        const gig = await Gig.findById(req.params.gigId)
-        
-        const newOrder = new Order ({
-            gigId: gig._id,
-            img: gig.cover,
-            title: gig.title,
-            buyerId: req.userId,
-            sellerId: gig.userId,
-            price: gig.price,
-            payment_intent: 'temporary',
-        })
-
-        await newOrder.save()
-        res.status(200).send('stripe-id-successful')
-    } catch (err) {
-        next(err)
-    }
-}
 
 export const getOrders = async (req, res, next)=>{
     try {
--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../middleware/jwt.js'
-import { createOrder, getOrders, intent } from '../controllers/order.controller.js'
+import { getOrders, intent } from '../controllers/order.controller.js'
 
 const router =  express.Router()
 
