74d128f8e4d80c52de53399b6fc7d3027d5a9196
Deadtargaryen <mannyetukudoh@gmail.com>
2024-01-23 10:37:29 +0100

    update

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const newRequest = axios.create({
-    baseURL:'https://creepy-umbrella-bee.cyclic.app/api', 
+    baseURL:'http://localhost:8800/api', 
     withCredentials: true,
 })
 
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const connect = async ()=>{
     }
 }
 
-app.use(cors({origin:['https://t-work-two.vercel.app, http://localhost:5173'], 
+app.use(cors({origin:'http://localhost:5173', 
 credentials:true
 }))
 // Handle preflight requests
