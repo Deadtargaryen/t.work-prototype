e26c9b3d87f73350c6e1a73e3507fa1f604867d5
Deadtargaryen <mannyetukudoh@gmail.com>
2024-01-24 14:19:08 +0100

    ...trial 1

--- api/server.js
+++ api/server.js
@@ -16,25 +16,28 @@ const app = express()
 dotenv.config()
 mongoose.set('strictQuery', true)
 
-const connect = async ()=>{
-    try{
+const connect = async () => {
+    try {
         await mongoose.connect(process.env.MONGO);
-    console.log('Connected to MongoDB!')
-    } catch(error){
-    console.log(error);
+        console.log('Connected to MongoDB!')
+    } catch (error) {
+        console.log(error);
     }
 }
+
 const port = process.env.PORT || 8800
 
-app.use(cors(
-// origin:'https://t-work-prototype.vercel.app', 
-// credentials:true
-))
+// Enable CORS for all routes
+app.use(cors({
+    origin: 'https://t-work-prototype.vercel.app',
+    credentials: true,
+}))
+
 // Handle preflight requests
 app.options('*', cors())
+
 app.use(express.json())
 app.use(cookieParser())
-
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
@@ -44,7 +47,7 @@ app.use('/api/conversations', conversationRoute)
 app.use('/api/order', orderRoute)
 app.use('/api/review', reviewRoute)
 
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
     const errorStatus = err.status || 500
     const errorMessage = err.message || 'something went wrong!'
 
@@ -53,5 +56,5 @@ app.use((err, req, res, next)=>{
 
 app.listen(port, () => {
     connect()
-console.log('Backend server is running!')
+    console.log('Backend server is running!')
 })
