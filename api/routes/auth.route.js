468feb8e8a97cfba1cb0086d56226569b0d714f7
Deadtargaryen <mannyetukudoh@gmail.com>
2024-01-27 19:07:22 +0100

    authRoute.js update with cors

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,17 @@
-import express from 'express'
-import { register, login, logout } from '../controllers/auth.controller.js'
+import express from 'express';
+import cors from 'cors';
+import { register, login, logout } from '../controllers/auth.controller.js';
 
-const router =  express.Router()
+const router = express.Router();
 
-router.post('/register', register)
-router.post('/login', login)
-router.post('/logout', logout)
+// Apply CORS middleware specifically to authRoute
+router.use(cors({
+    origin: 'https://t-work-prototype.vercel.app',
+    credentials: true,
+}));
 
-export default router
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', logout);
+
+export default router;
