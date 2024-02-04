01400ed4fa267ad66946ea6505343236ac106bdf
Deadtargaryen <mannyetukudoh@gmail.com>
2024-02-04 21:56:08 +0100

    ...

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -81,33 +81,15 @@ const Navbar = () => {
         <>
             <hr />
             <div className="menu">
-                <Link className='link menuLink' to ="/">
-                    Graphics & Design
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Video & Animation
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Writing & Translation
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    AI Services
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Digital Marketing
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Music & Audio
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Programming & Tech
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Business
-                </Link>
-                <Link className='link menuLink' to ="/">
-                    Lifestyle
-                </Link>
+                <Link className='link menuLink' to ="/">Graphics & Design</Link>
+                <Link className='link menuLink' to ="/">Video & Animation</Link>
+                <Link className='link menuLink' to ="/">Writing & Translation</Link>
+                <Link className='link menuLink' to ="/">AI Services</Link>
+                <Link className='link menuLink' to ="/">Digital Marketing</Link>
+                <Link className='link menuLink' to ="/">Music & Audio</Link>
+                <Link className='link menuLink' to ="/">Programming & Tech</Link>
+                <Link className='link menuLink' to ="/">Business</Link>
+                <Link className='link menuLink' to ="/">Lifestyle</Link>
             </div>
             <hr />
         </>
